c25b17a682d0726de813fffda64e6f3dc6054495
rooat <welling1234@gmail.com>
2020-01-11 12:26:37 +0800

    u

--- class/Tool.js
+++ class/Tool.js
@@ -5,9 +5,9 @@ class Tool{
     }
     async updateWalletVersion(){
         await config.db.WalletVersion({
-            "versionCode":"42",
-            "version":"1.3.0",
-            "url":"http://etz.oss-cn-hongkong.aliyuncs.com/easyetz-1_3_0_b42.apk",
+            "versionCode":"7",
+            "version":"1.0",
+            "url":"https://auchain.oss-cn-shenzhen.aliyuncs.com/AUChain.apk",
             "content":"优化发现页面加载，解决使用中发现的问题. （ Optimizing when loading the startup pag）",
             "createAt":new Date()
         }).save();
